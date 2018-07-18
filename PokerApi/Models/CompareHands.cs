3d6015397a1a767cad485177dbdaccbca77c42aa
Eric <arboureric@gmail.com>
2018-07-17 20:22:36 -0400

    add method to assign values and types to list of hands

---
+++ PokerApi/Models/CompareHands.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokerApi.Models
+{
+    public class CompareHands
+    {
+        public List<HandWithData> AssignTypesAndValues(List<List<string>> hands)
+        {
+            return hands.Select(hand => new HandWithData { Hand = hand, Type = "test", Value = 10000000 }).ToList();
+        }
+    }
+
+    public class HandWithData
+    {
+        public List<string> Hand;
+        public string Type;
+        public int Value;
+    }
+}
--- PokerApi/Models/DoStuff.cs
+++ PokerApi/Models/DoStuff.cs
@@ -18,13 +18,17 @@ namespace PokerApi.Models
         public async void AddShowdown()
         {
             var showdown = new Showdown { Deck = "[\"2H\", \"3H\", \"4H\", \"5H\", \"6H\", \"7H\", \"9H\", \"TH\", \"JH\", \"QH\", \"KH\", \"AH\", \"2D\", \"3D\", \"5D\", \"6D\", \"7D\", \"8D\", \"9D\", \"TD\", \"KD\", \"AD\", \"2S\", \"3S\", \"4S\", \"5S\", \"7S\", \"8S\", \"9S\", \"JS\", \"QS\", \"KS\", \"3C\", \"4C\", \"5C\", \"6C\", \"7C\", \"8C\", \"9C\", \"TC\", \"KC\", \"AC\"]", Hand1 = "[\"QD\", \"8H\", \"TS\", \"JC\", \"4D\"]", Hand1Type = "High Card", Hand2 = "[\"2C\", \"JD\", \"6S\", \"QC\", \"AS\"]", Hand2Type = "High Card", Result = "LT" };
+
             var optionsBuilder = new DbContextOptionsBuilder<ShowdownContext>();
             optionsBuilder.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
 
             using (var context = new ShowdownContext(optionsBuilder.Options))
             {
-                var cardValues = new CardValues();
-                var test = cardValues.GetFiveKVal("A");
+                var hand1 = new List<string> { "QD", "8H", "TS", "JC", "4D" };
+                var hand2 = new List<string> { "2C", "JD", "6S", "QC", "AS" };
+                var hands = new List<List<string>> { hand1, hand2 };
+                var compareHands = new CompareHands();
+                var test = compareHands.AssignTypesAndValues(hands);
                 var showdownsController = new ShowdownsController(context);
                 await showdownsController.PostShowdown(showdown);
             }
