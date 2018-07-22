d5ceec9217fcc857adf32b237b76c7bd01b3a127
Eric <arboureric@gmail.com>
2018-07-22 18:56:12 -0400

    add TestPokerApi Xunit project

---
+++ TestPokerApi/HighCardHands.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TestPokerApi
+{
+    public class HighCardHands
+    {
+        // [Theory]
+        // [InlineData(new List<List<string>> { new List<string> { "AD", "AH", "AD", "AD", "3D" }, new List<string> { "2C", "3C", "4C", "5C", "7C" } })]
+        public void TestHighCardHands(List<List<string>> hands)
+        {
+            Assert.Equal(4, 2 + 2);
+        }
+    }
+}
